851b62e691147535d292d4306d6065f14391149c
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-11-03 18:30:52 -0500

    chore: upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '.chemist',
+      'coverage',
+      'dist',
+      '.husky',
+      'lib',
+      'node_modules',
+      'tmp',
+      '.vite',
+    ],
+  },
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.strict],
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': ['warn'],
+    },
+  }
+);
